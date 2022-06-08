e126722e2e0d31acd6ad8d26520a31d5e48de6d9
Galkin Maxim <maxim.galkin.1991@gmail.com>
2022-06-08 16:38:21 +0300

    changes
    Не работают компоненты

--- script/main.js
+++ script/main.js
@@ -1,4 +1,5 @@
-"use strict";
+// @ts-nocheck
+
 
 const BASE_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 const GOODS = `${BASE_URL}/catalogData.json`;
@@ -6,4 +7,47 @@ const BASKETGOODS = `${BASE_URL}/getBasket.json`;
 
 function service(url) {
     return fetch(url).then(res => res.json())
+}
+
+window.onload = () => {
+    const app = new Vue({
+        el: '#root',
+
+        data: {
+            list: [],
+            searchValue: '',
+            basketWindowVisible: false
+        },
+
+        methods: {
+            basketWindowVisibleChange() {
+                this.basketWindowVisible = !this.basketWindowVisible
+            }
+        },
+
+        mounted() {
+            service(GOODS).then(data => {
+                this.list = data;
+                return data
+            })
+        },
+
+        computed: {
+            getCount() {
+                return this.list.reduce((prev, { price }) => prev + price, 0)
+            },
+
+            filteredList() {
+                return this.list.filter(({ product_name }) => {
+                    return product_name.match(RegExp(this.searchValue, 'gui'))
+                })
+            },
+        }
+    });
+
+    Vue.component("rte", {
+        template: '
+            
+        '
+    })
 }
