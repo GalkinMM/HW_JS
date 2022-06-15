fc7b9d8ce7a4f43008f6bba0c31ef10eaaa92962
Galkin Maxim <maxim.galkin.1991@gmail.com>
2022-06-16 00:08:06 +0300

    server work

--- script/main.js
+++ script/main.js
@@ -1,9 +1,10 @@
 // @ts-nocheck
 "use strict"
 
-const BASE_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
-const GOODS = `${BASE_URL}/catalogData.json`;
-const BASKETGOODS = `${BASE_URL}/getBasket.json`;
+// const BASE_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
+const BASE_URL = 'http://localhost:8000/';
+const GOODS = `${BASE_URL}catalogData.json`;
+const BASKETGOODS = `${BASE_URL}getBasket.json`;
 
 function service(url) {
     return fetch(url).then(res => res.json())
--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public');)
-app.use(cors());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    console.log('request body - \n', req.body);
+    console.log('get response');
+    res.send('test');
 });
 
 app.listen('8000', () => {
